cd1f8c462e764219a77977a21ca9600baae770a4
flo <florianhagemann@gmx.net>
2022-03-22 20:30:07 +0100

    server socket stuff implemented

--- src/v1_0/Server.java
+++ src/v1_0/Server.java
@@ -1,5 +1,39 @@
 package v1_0;
 
-public class Server {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	
+	private ServerSocket ss;
+	private Socket s;
+	private DataOutputStream dOut;
+	
+	void Server() {
+		
+		try {
+			
+			// Create sockets
+			ss = new ServerSocket(6666);
+			s = ss.accept();
+			
+			// Create outputstream and send a string
+			dOut = new DataOutputStream(s.getOutputStream());
+			dOut.writeUTF("hi client");
+			dOut.flush();
+			
+			// Close everything
+			dOut.close();
+			s.close();
+			ss.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
